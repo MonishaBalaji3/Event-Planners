1559aadca1a7fceb45b2a2a116d24e8a316e7265
Monisha2003 <121876253+MonishaBalaji3@users.noreply.github.com>
2024-10-30 14:18:52 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,34 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>S</strong>
+                        <strong>M</strong>
+                        <strong>S</strong>
+                        
+                    </p>
+
+                    
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    For more content<br>Follow us on<br> Instagram : sms_events<br>Facebook : SMS Events<br>Youtube : SMS Events
+
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/ig.png">
+                        <img src = "images/facebook.png">
+                        <img src = "images/yt.png">
+                        
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
